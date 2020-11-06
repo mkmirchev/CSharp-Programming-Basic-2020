156b3a398dc7a16f1aac875f0f69d29d428ae81d
mkmirchev <mkmirchev@abv.bg>
2020-11-07 00:12:03 +0200

    update

---
+++ AreaOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = a * a;
+                Console.WriteLine("{0:F3}",area);
+            }
+            else if (input == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = (a * b);
+                Console.WriteLine("{0:F3}", area);
+            }
+            else if (input == "circle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = a * a*Math.PI;
+                Console.WriteLine("{0:F3}", area);
+            }
+            else if (input == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = (a * b) / 2;
+                Console.WriteLine("{0:F3}", area);
+            }
+        }
+    }
+}
---
+++ Number 100-200/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Number_100_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (100 > a)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (200 < a)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+            else
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+        }
+    }
+}
