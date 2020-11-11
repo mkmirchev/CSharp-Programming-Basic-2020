efca6f8765f48294aa7ce20ed0e953bb89233d4d
mkmirchev <mkmirchev@abv.bg>
2020-11-11 23:55:37 +0200

    11.11.2020

---
+++ Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cinemaType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colunms = int.Parse(Console.ReadLine());
+            int cinemaCapacity = rows * colunms;
+            double ticketPrice = 0.0;
+            switch (cinemaType)
+            {
+                case "Premiere":
+                    ticketPrice = 12.0;
+                    break;
+                case "Normal":
+                    ticketPrice = 7.5;
+                    break;
+                case "Discount":
+                    ticketPrice = 5.0;
+                    break;
+                default:
+                    break;
+            }
+            double totalIncome = (double)cinemaCapacity * ticketPrice;
+            Console.WriteLine("{0:F2} leva", totalIncome);
+        }
+    }
+}
---
+++ SummerOutfit/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+            switch (input)
+            {
+                case "Morning":
+                    if (temperature >=0 && temperature <= 18)
+                    {
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+
+                    }
+                    else if (temperature > 18 && temperature <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (temperature > 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    break;
+                case "Afternoon":
+                    if (temperature >= 0 && temperature <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+
+                    }
+                    else if (temperature > 18 && temperature <= 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    else if (temperature > 24)
+                    {
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                    }
+                    break;
+                case "Evening":
+                    if (temperature >= 0 && temperature <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+
+                    }
+                    else if (temperature > 18 && temperature <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (temperature > 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            string output = "It's " + temperature + " degrees, get your " + outfit + " and " + shoes + ".";
+            Console.WriteLine(output);
+        }
+    }
+}
