bc37a8fdcb40378f364f7688fca77c1da8c8bcdd
mkmirchev <mkmirchev@abv.bg>
2020-11-12 19:57:14 +0200

    Homework 12.11.2020

---
+++ FishingBoat/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int persons = int.Parse(Console.ReadLine());
+            int rent = 0;
+            double discount = 1;
+            double bonusDiscount = 1;
+            switch (season)
+            {
+                case "Autumn":
+                    rent = 4200;
+                    break;
+                case "Summer":
+                    rent = 4200;
+                    break;
+                case "Spring":
+                    rent = 3000;
+                    break;
+                case "Winter":
+                    rent = 2600;
+                    break;
+                default:
+                    break;
+            }
+            if (persons <=6)
+            {
+                discount = 0.9;
+            }
+            else if (persons > 6 && persons <=11)
+            {
+                discount = 0.85;
+            }
+            else if (persons >11)
+            {
+                discount = 0.75;
+            }
+            if (season != "Autumn" && persons % 2 == 0)
+            {
+                bonusDiscount = 0.95;
+            }
+           double tripPrice = rent * discount * bonusDiscount;
+            if (budget >= tripPrice)
+            {
+                double moneyLeft = budget - tripPrice;
+                Console.WriteLine("Yes! You have {0:F2} leva left.",moneyLeft);
+            }
+            else if (budget < tripPrice)
+            {
+                double moneyNeeded = tripPrice - budget;
+                Console.WriteLine("Not enough money! You need {0:F2} leva.",moneyNeeded);
+            }
+        }
+    }
+}
---
+++ newHome/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace newHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string flower = Console.ReadLine();
+            int amount = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double discount = 1;
+            double flowerPrice = 0;
+            switch (flower)
+            {
+                case "Roses":
+                    flowerPrice = 5;
+                    if (amount > 80)
+                    {
+                        discount = 0.9;
+                    }
+                    break;
+                case "Dahlias":
+                    flowerPrice = 3.80;
+                    if (amount > 90)
+                    {
+                        discount = 0.85;
+                    }
+                    break;
+                case "Tulips":
+                    flowerPrice = 2.8;
+                    if (amount > 80)
+                    {
+                        discount = 0.85;
+                    }
+                    break;
+                case "Narcissus":
+                    flowerPrice = 3;
+                    if (amount < 120)
+                    {
+                        discount = 1.15;
+                    }
+                    break;
+                case "Gladiolus":
+                    flowerPrice = 2.5;
+                    if (amount < 80)
+                    {
+                        discount = 1.2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            double moneyNeeded = amount * flowerPrice * discount;
+            if (moneyNeeded <= budget)
+            {
+                double moneyLeft = budget - moneyNeeded;
+                Console.WriteLine("Hey, you have a great garden with {0} {1} and {2:F2} leva left.",amount,flower,moneyLeft);
+            }
+            else if (moneyNeeded > budget)
+            {
+                double neededMoney = moneyNeeded - budget;
+                Console.WriteLine("Not enough money, you need {0:F2} leva more.",neededMoney);
+            }
+        }
+    }
+}
